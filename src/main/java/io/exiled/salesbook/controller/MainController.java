38fdcdcc8f7f7f2285db08e7bc7ce8158f0f4907
Exiled Rain <vassilimoskaljov@gmail.com>
2019-12-10 20:30:27 +0200

    Added basic functionality

--- src/main/java/io/exiled/salesbook/controller/MainController.java
+++ src/main/java/io/exiled/salesbook/controller/MainController.java
@@ -1,19 +1,63 @@
 package io.exiled.salesbook.controller;
 
+import io.exiled.salesbook.model.Sale;
+import io.exiled.salesbook.repos.SaleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+    private SaleRepo saleRepo;
 
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String getStarted() {
         return "main";
     }
 
-    @RequestMapping("/status")
-    public String getStatus() {
-        return "status";
+    @GetMapping("/sales")
+    public String getSales(Model model) {
+        List<Sale> sales = saleRepo.findAll();
+        model.addAttribute("sales", sales);
+        return "sales";
+    }
+
+    @PostMapping("/sales")
+    public String addSale(
+            @RequestParam String name,
+            @RequestParam(name = "email", required = false) String email,
+            @RequestParam(name = "cat", required = false) String cat,
+            @RequestParam int totalCost,
+            @RequestParam(name = "description", required = true, defaultValue = "In progress...") String description,
+            @RequestParam boolean isDone,
+            Model model
+    ) {
+        Sale sale = new Sale(totalCost, name, cat, email, description, isDone);
+//        String
+        saleRepo.save(sale);
+        List<Sale> sales = saleRepo.findAll();
+        model.addAttribute("sales", sales);
+
+        return "sales";
+    }
+
+    @PostMapping("search")
+    public String search(
+            @RequestParam String ename,
+            Model model
+    ) {
+        List<Sale> saleList = saleRepo.findByName(ename);
+        model.addAttribute("saleList", saleList);
+        return "search";
+    }
+
+    @Autowired
+    public void setSaleRepo(SaleRepo saleRepo) {
+        this.saleRepo = saleRepo;
     }
 }
---
+++ src/main/java/io/exiled/salesbook/model/Sale.java
@@ -0,0 +1,38 @@
+package io.exiled.salesbook.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity(name = "sale")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private int totalCost;
+    private String name;
+    private String cat;
+    private String email;
+    private String description;
+    private boolean isDone;
+
+    public Sale() {
+    }
+
+    public Sale(int totalCost, String name, String cat, String email, String description, boolean isDone) {
+        this.totalCost = totalCost;
+        this.name = name;
+        this.cat = cat;
+        this.email = email;
+        this.description = description;
+        this.isDone = isDone;
+    }
+}
