ac6b2368d62206600386e7313b9e63cf15fb5b8b
Exiled Rain <vassilimoskaljov@gmail.com>
2020-01-19 19:36:23 +0200

    Disabled security

--- src/main/java/io/exiled/salesbook/config/MvcConfig.java
+++ src/main/java/io/exiled/salesbook/config/MvcConfig.java
@@ -3,7 +3,6 @@ package io.exiled.salesbook.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,9 +10,9 @@ public class MvcConfig implements WebMvcConfigurer {
     @Value("${upload.path}")
     private String uploadPath;
 
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-    }
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/login").setViewName("login");
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
